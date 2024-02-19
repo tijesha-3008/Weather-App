2dd73f215ca547a749ec4d03219469c204ab25af
Tijesha Roy <103642078+tijesha-3008@users.noreply.github.com>
2024-02-19 22:12:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TempApp from './component/TempApp.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <TempApp/>
+    </div>
+  );
+}
+
+export default App;
